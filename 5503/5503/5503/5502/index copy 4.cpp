484a5d3078b09c36a1c674184ba5e9d8f8eb793f
AC1D <moonwatcher445@gmail.com>
2026-01-20 11:35:05 -0500

    Delete 5503/5503/5503 directory

--- 5503/5503/5503/5502/index copy 4.cpp
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-	<key>URL</key>
-	<string>https://github.com/AC1DST4R/Web-Ports/blob/main/beautify/style%20(1).css</string>
-</dict>
-</plist>
